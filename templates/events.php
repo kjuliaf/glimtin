5050944a744bc40cfdfe4f5da885b426ce4ded74
Hanna Böös <ens21hbs@cs.umu.se>
2025-05-06 14:39:20 +0200

    har börjat på event sidan, ett kort är i konstruktion :)

--- templates/events.php
+++ templates/events.php
@@ -7,10 +7,24 @@ Template Name: Event
 <?php get_header(); ?>
 
 <main>
-	<!-- Replace this below when you start working ... -->
-	<h1 class="page-heading"><?php the_field("Intro_text"); ?></h1>
-	<p>Test2</p>
+    <!-- Titelsektion -->
+    <section class="intro_title">
+		<h1 class="h1"><?php the_field('title_title'); ?></h1>
+        <p><?php the_field('intro_text'); ?></p>
+    </section>
+    
+    <!-- Eventsektion med flexbox för kort -->
+    <section class="event_cards">
+        <!-- Här kan du skapa dynamiska eventkort med en loop -->
+        <div class="event_card">
 
+			<div class="overlay_event_card"></div>
+			<p class="event_card_title"><?php the_field('event_title'); ?></p>
+		</div>
+        <div class="event_card">Event 2</div>
+        <div class="event_card">Event 3</div>
+        <!-- Exempel på kort, du kan använda en loop för att hämta dynamiskt innehåll här -->
+    </section>
 </main>
 
 <?php get_footer(); ?>
