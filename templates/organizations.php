f1c2063dafef3d15f6902192b31b510da075caf0
JessicaLundberg <159144381+JessicaLundberg@users.noreply.github.com>
2025-05-06 11:53:17 +0200

    Hero test push

--- templates/organizations.php
+++ templates/organizations.php
@@ -7,8 +7,11 @@ Template Name: Företag
 <?php get_header(); ?>
 
 <main>
-	<!-- Replace this below when you start working ... -->
-	<h1 class="page-heading"><?php the_title(); ?></h1>
+	<!-- Hero Section -->
+	<section class="hero overlay" style="background-image: url('<?php the_field('hero_image'); ?>');">
+		<p class="hero__title"><?php the_field('hero_title'); ?></p>
+		<p class="hero__text"><?php the_field('hero_text'); ?></p>
+	</section>
 </main>
 
 <?php get_footer(); ?>
