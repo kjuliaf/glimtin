1a3e33866860f42ff921871174e6cb58f1a6e1c2
Emmy Renlund <emre0053@ad.umu.se>
2025-05-13 15:02:25 +0200

    Third commit: Added critical section

--- templates/about.php
+++ templates/about.php
@@ -8,7 +8,6 @@ Template Name: Om oss
 
 <main>
 	<!-- Replace this below when you start working ... -->
-	<h1 class="page-heading"><?php the_field('test'); ?></h1>
 	<section class="ceo-info-section">
 		<div>
 			<img src="<?php the_field('profile'); ?>" alt="Bild på ägaren johanna" class="ceo-info-image"/>
@@ -19,7 +18,7 @@ Template Name: Om oss
 				<p><?php the_field('ceoInfoText'); ?></p>
 			</div>
 			<div>
-				<h3>Kontakt</h3>
+				<h2>Kontakt</h2>
 				<p><?php the_field('ceoInfoContact'); ?></p>
 			</div>
 		</div>
@@ -43,7 +42,23 @@ Template Name: Om oss
 	</section>
 
 	<section class="critical-section">
-		
+		<div>
+			<img src="<?php the_field('image2'); ?>" alt="Kort statistik" class="critical-image"/>
+		</div>
+		<div class="critical-text">
+			<div>
+				<h1><?php the_field('criticalTitle'); ?></h1>
+			</div>
+			<div>
+				<p><?php the_field('criticalText'); ?></p>
+			</div>
+			<div>
+				<a href="<?php the_field('criticalLink'); ?>" class="critical-link">
+					<span class="link">Läs mer</span>
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow.svg" alt="Pil" />
+				</a>
+			</div>
+		</div>
 	</section>
 	
 	<div></div>
