6591b6f82701f2835d2a1e40e80ff73b409b4772
Julia Forsberg <103151478+kjuliaf@users.noreply.github.com>
2025-05-26 11:16:02 +0200

    Fixed info text and collaborators

--- templates/front-page.php
+++ templates/front-page.php
@@ -12,20 +12,17 @@ get_header();
 
 	<!-- Who Are We Section -->
 	<section class="who-are-we">
-		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/circuit-left.png" alt="Kretskort figur vänster" class="who-are-we__img-left" />
 		<div class="who-are-we__box">
 			<h2 class="who-are-we__box-title"><?php the_field('who_are_we_title'); ?></h2>
-			<p class="who-are-we__box-text"><?php the_field('who_are_we_text'); ?></p>
-			<a href="/om-oss" class="who-are-we__box-link">
-				<span class="link">Läs mer</span>
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow.svg" alt="Pil" />
-			</a>
+			<div class="who-are-we__box-text">
+				<p><?php the_field('who_are_we_text'); ?></p>
+				<a href="/om-oss" class="who-are-we__box-link">
+					<span class="link">Läs mer</span>
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow.svg" alt="Pil" />
+				</a>
+			</div>
 		</div>
-		<div class="who-are-we__img-right">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/circuit-right-1.png" alt="Kretskort figur höger" class="who-are-we__img-right-1">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/circuit-right-2.png" alt="Kretskort figur höger" class="who-are-we__img-right-2">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/circuit-right-3.png" alt="Kretskort figur höger" class="who-are-we__img-right-3">
-		</div>
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/circuit.png" alt="Kretskort figur" class="who-are-we__img">
 	</section>
 
 	<!-- Partners Section -->
@@ -35,18 +32,33 @@ get_header();
 			<hr class="line" />
 		</div>
 		<!-- Import Partners Logos -->
-		<div class="partners__logos">
-			<?php
-			$partner_count = 10;
-			for( $i = 1; $i <= $partner_count; $i++ ):
-				$logo = get_field('partner_logo_' . $i);
-				if( $logo ):
-			?>
-			<img src="<?php echo esc_url($logo); ?>" alt="photo); ?>" class="partners__logos-img" />
-			<?php
-				endif;
-			endfor;
-			?>
+		<div class="partners__logos-outer">
+			<div class="partners__logos partners__logos--animate">
+				<?php
+				$partner_count = 10;
+				for( $i = 1; $i <= $partner_count; $i++ ):
+					$logo = get_field('partner_logo_' . $i);
+					if( $logo ):
+				?>
+
+				<img src="<?php echo esc_url($logo); ?>" alt="photo); ?>" class="partners__logos-img" />
+				<?php
+					endif;
+				endfor;
+				?>
+				<?php
+				$partner_count = 10;
+				for( $i = 1; $i <= $partner_count; $i++ ):
+					$logo = get_field('partner_logo_' . $i);
+					if( $logo ):
+				?>
+
+				<img src="<?php echo esc_url($logo); ?>" alt="photo); ?>" class="partners__logos-img" />
+				<?php
+					endif;
+				endfor;
+				?>
+			</div>
 		</div>
 	</section>
 
