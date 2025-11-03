57dabaefd0c9badaf50a12f1e31bcf4a8ea86136
Julia Forsberg <103151478+kjuliaf@users.noreply.github.com>
2025-11-03 10:42:52 +0100

    Start of fix of width/padding/margin of sections

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -5,32 +5,37 @@ function About() {
 		<section>
 			<div
 				id="om"
-				className="bg-background-medium mt-14 flex flex-col-reverse gap-2 rounded-t-4xl py-20 md:flex-row md:items-center md:gap-8 lg:gap-12"
+				className="bg-background-medium section-padding-y section-padding-x mt-14 flex justify-center rounded-t-4xl md:!px-0"
 			>
-				<img
-					className="m-5 aspect-3/2 max-w-120 rounded-3xl object-cover md:m-0 md:aspect-auto md:h-auto md:w-1/2 md:flex-shrink md:rounded-l-none md:rounded-r-3xl"
-					src={CityImage}
-					alt=""
-				/>
-				<div className="m-5 md:mr-10 md:max-w-180">
-					<h2 className="text-3xl md:text-4xl lg:text-5xl">
-						Ett nätverk där tjejer får veta mer om ingenjörsyrket
-					</h2>
-					<p className="mt-7">
-						Glimtin öppnar dörrar för unga tjejer att upptäcka och
-						utveckla sitt intresse för ingenjörsyrket. Genom ett
-						stöttande nätverk får tjejer möjlighet att upptäcka och
-						engagera sig i tekniska ämnen på ett inspirerande och
-						praktiskt sätt.
-					</p>
-					<p className="mt-6">
-						Här möts elever, företag och sponsorer i Umeå som alla
-						är engagerade i att främja jämställdhet och mångfald
-						inom ingenjörsbranschen.
-					</p>
+				<div className="flex max-w-130 flex-col-reverse items-center gap-2 sm:max-w-none md:flex-row md:items-center md:gap-8 lg:gap-12">
+					{" "}
+					{/* center */}
+					<img
+						className="m-5 aspect-3/2 rounded-3xl object-cover md:m-0 md:aspect-auto md:h-auto md:w-1/2 md:max-w-120 md:flex-shrink md:rounded-l-none md:rounded-r-3xl"
+						src={CityImage}
+						alt="Foto över Umeå Väven"
+					/>
+					<div className="mb-5 md:mr-10">
+						<h2 className="section-heading-size">
+							Ett nätverk där tjejer får veta mer om
+							ingenjörsyrket
+						</h2>
+						<p className="mt-7">
+							Glimtin öppnar dörrar för unga tjejer att upptäcka
+							och utveckla sitt intresse för ingenjörsyrket. Genom
+							ett stöttande nätverk får tjejer möjlighet att
+							upptäcka och engagera sig i tekniska ämnen på ett
+							inspirerande och praktiskt sätt.
+						</p>
+						<p className="mt-6">
+							Här möts elever, företag och sponsorer i Umeå som
+							alla är engagerade i att främja jämställdhet och
+							mångfald inom ingenjörsbranschen.
+						</p>
+					</div>
 				</div>
 			</div>
-			<div className="bg-primary-dark p-12 px-5 text-lg md:px-12">
+			<div className="bg-primary-dark section-padding-x py-12 text-lg">
 				<h3 className="!text-secondary-light">Vår vision</h3>
 				<p className="text-neutral-000 mt-2 text-2xl md:text-3xl">
 					“Att förändra bilden av teknik och göra den till en plats
--- src/sections/Background.jsx
+++ src/sections/Background.jsx
@@ -1,5 +1,25 @@
 function Background() {
-	return <section>Bakgrund</section>
+	return (
+		<div className="section-wrapper">
+			<section className="section-padding-y-lg section-padding-x">
+				<h2 className="!text-accent section-heading-size">Bakgrund</h2>
+				<p className="mt-7">
+					Glimtin grundades med en tanke om att vilja ge vidare till
+					kommande elever av de tankar och kunskap vi fått längs
+					vägen. Vi vill visa hur mycket som finns att jobba med och
+					konkretisera vad detta kan vara. Vi vill även ge elever en
+					inblick i företagslivet som stort och visa hur behövda de
+					är. Med företag i ryggen gör vi detta som mellanhanden
+					mellan elever och företag.
+				</p>
+				<p className="mt-6">
+					Vi på glimtin är ett team unga tjejer som själva gått vägen
+					från grundskola till universitetsutbildning och viss del
+					arbetsliv.
+				</p>
+			</section>
+		</div>
+	)
 }
 
 export default Background
