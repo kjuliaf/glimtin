546875a23f4f3cd14158d153bcdef627c88901b4
Julia Forsberg <103151478+kjuliaf@users.noreply.github.com>
2025-11-04 09:10:04 +0100

    Added Companies and Footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,42 @@
+import LinkedIn from "../assets/icons/linkedin.svg"
+import Instagram from "../assets/icons/instagram.svg"
+import TikTok from "../assets/icons/tiktok.svg"
+
 function Footer() {
-	return <footer id="#kontakt">Kontakta oss</footer>
+	return (
+		<footer
+			id="#kontakt"
+			className="section-padding-y-lg section-padding-x section-center bg-primary-dark rounded-t-4xl"
+		>
+			<div className="section-wrapper">
+				<h2 className="section-heading-size-sm !text-secondary-light">
+					Kontakta oss
+				</h2>
+				<p className="text-neutral-000 mt-6 mb-8">
+					Om du har några frågor, tveka inte på att höra av dig till
+					oss
+				</p>
+				<a
+					className="text-neutral-000 underline"
+					href="mailto:kontakt@glimtin.se"
+				>
+					kontakt@glimtin.se
+				</a>
+
+				<div className="mt-12 mb-10 flex gap-1">
+					<a href="https://www.linkedin.com/company/glimtin">
+						<img className="w-14" src={LinkedIn} alt="LinkedIn" />
+					</a>
+					<a href="https://www.instagram.com/glimtin/">
+						<img className="w-14" src={Instagram} alt="Instagram" />
+					</a>
+					<a href="">
+						<img className="w-14" src={TikTok} alt="TikTok" />
+					</a>
+				</div>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
--- src/sections/Companies.jsx
+++ src/sections/Companies.jsx
@@ -1,5 +1,27 @@
 function Companies() {
-	return <section>För företag</section>
+	return (
+		<section className="section-padding-y-lg section-padding-x section-center">
+			<div className="section-wrapper mb-5">
+				<h2 className="section-heading-size !text-accent">
+					För företag
+				</h2>
+				<p className="mt-7">
+					Vi på Glimtin vill tillsammans med er skapa framtidens
+					ingenjörer. Genom ert engagemang och stöd kan vi ge unga
+					tjejer möjlighet att upptäcka, prova och inspireras av
+					teknik och ingenjörsyrket i praktiken.{" "}
+				</p>
+				<h3 className="!text-accent mt-10 text-xl md:text-2xl">
+					Frågor & svar
+				</h3>
+				<div>
+					<p className="mt-5">Vad innebär det att medverka?</p>
+					<p className="mt-3">Vilken skillnad kan ni göra?</p>
+					<p className="mt-3">Hur blir man partner?</p>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Companies
