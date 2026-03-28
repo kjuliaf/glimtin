1bc456f783e30c28ac1f3176afc8316a94ecf13b
Julia Forsberg <103151478+kjuliaf@users.noreply.github.com>
2026-03-29 00:29:28 +0100

    Added workshop event info on start page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import Logo from "../assets/images/logo.svg?react"
 import Button from "./UI/Button"
+import ArrowCurved from "../assets/icons/arrow-curved.svg"
+import { useLocation } from "react-router-dom"
 
 function Header() {
+	const location = useLocation()
+	console.log(location.pathname)
+
 	return (
 		<header className="flex items-center justify-between px-4 py-2 sm:px-6">
 			<a href="/">
@@ -10,10 +15,20 @@ function Header() {
 			<nav>
 				<ul className="text-primary mr-2 flex items-center gap-4 text-lg">
 					<li>
-						<div>
+						<div className="relative">
 							<Button className="leading-snug" link="/event">
 								Event
 							</Button>
+							{location.pathname != "/event" && (
+								<div className="text-primary-dark absolute top-11 right-14 flex gap-0.5 text-sm">
+									<p>Missa inte vårt workshopevent!</p>
+									<img
+										className="w-8"
+										src={ArrowCurved}
+										alt="Pil"
+									/>
+								</div>
+							)}
 						</div>
 					</li>
 				</ul>
